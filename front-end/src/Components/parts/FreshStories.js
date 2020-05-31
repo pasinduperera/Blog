7ce825f3a945f1f0731b0287fcfb555518abbb85
root <root@localhost.localdomain>
2020-05-31 14:53:51 -0400

    all modifications

---
+++ front-end/src/Components/parts/FreshStories.js
@@ -0,0 +1,75 @@
+import React from 'react'
+
+const FrechStories =() =>{
+
+    return(
+        <section class="fresh-section on-trend-mode">
+			<div class="container">
+				<div class="title-section text-center">
+					<h1>Fresh Stories</h1>
+				</div>
+				<div class="fresh-box owl-wrapper">
+					
+					<div class="owl-carousel" data-num="2">
+					
+						<div class="item">
+							<div class="news-post article-post">
+								<div class="image-holder">
+									<img src="upload/blog/home5/a2.jpg" alt="" />
+								</div>
+								<a class="text-link" href="#">Travel</a>
+								<h2><a href="single-post.html">Vivamus vestibulum ntulla necante.</a></h2>
+								<ul class="post-tags">
+									<li>3 days ago</li>
+									<li><a href="#">3 comments</a></li>
+									<li>by <a href="#">John Smith</a></li>
+								</ul>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra donec nec justo eget felis facilisis fermentum ... </p>
+								<a class="text-link" href="single-post.html">Read More</a>
+							</div>
+						</div>
+					
+						<div class="item">
+							<div class="news-post article-post">
+								<div class="image-holder">
+									<img src="upload/blog/home5/a3.jpg" alt="" />
+								</div>
+								<a class="text-link" href="#">Food</a>
+								<h2><a href="single-post.html">Praesent placerat risus quis eros.</a></h2>
+								<ul class="post-tags">
+									<li>3 days ago</li>
+									<li><a href="#">3 comments</a></li>
+									<li>by <a href="#">John Smith</a></li>
+								</ul>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra donec nec justo eget felis facilisis fermentum ... </p>
+								<a class="text-link" href="single-post.html">Read More</a>
+							</div>
+						</div>
+					
+						<div class="item">
+							<div class="news-post article-post">
+								<div class="image-holder">
+									<img src="upload/blog/home5/a4.jpg" alt="" />
+								</div>
+								<a class="text-link" href="#">Food</a>
+								<h2><a href="single-post.html">Morbi in sem quisdui placerat ornare.</a></h2>
+								<ul class="post-tags">
+									<li>3 days ago</li>
+									<li><a href="#">3 comments</a></li>
+									<li>by <a href="#">John Smith</a></li>
+								</ul>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra donec nec justo eget felis facilisis fermentum ... </p>
+								<a class="text-link" href="single-post.html">Read More</a>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+    )
+
+}
+
+export default FrechStories;
